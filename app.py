d97a5615dd2c7cfe8435a9f615715c92a0760fec
Shiva Kumar P <shivakumarp943@gmail.com>
2026-05-22 14:15:20 +0530

    Testing automated deployment

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Pipeline Successfully Triggered"
+    return "FULLY AUTOMATED VMSS DEPLOYMENT SUCCESSFUL"
 
 app.run(host="0.0.0.0", port=5000)
